181b25273712c30681622f4aa6def114c7a21ab1
Brian Gunter <brigun@gmail.com>
2016-11-23 08:26:06 -0600

    initial commit for imp project

---
+++ src/main/java/com/code/brigun/imp/ImpappApplication.java
@@ -0,0 +1,12 @@
+package com.code.brigun.imp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImpappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImpappApplication.class, args);
+	}
+}
